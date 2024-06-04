30ebe975e09ba00a9680580b370356d6be454297
leeshinbi <tlsql6831@naver.com>
2024-06-04 21:21:01 +0900

    ✨ Redis 기능 추가
    캐싱 및 redis 기능 추가

--- sns/src/main/java/com/fastcampus/sns/service/UserService.java
+++ sns/src/main/java/com/fastcampus/sns/service/UserService.java
@@ -4,11 +4,13 @@ import com.fastcampus.sns.exception.SnsApplicationException;
 import com.fastcampus.sns.exception.enums.ErrorCode;
 import com.fastcampus.sns.model.dto.User;
 import com.fastcampus.sns.model.entity.UserEntity;
+import com.fastcampus.sns.repository.UserCacheRepository;
 import com.fastcampus.sns.repository.UserEntityRepository;
 import com.fastcampus.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,7 @@ public class UserService {
 
 	private final UserEntityRepository userEntityRepository;
 	private final BCryptPasswordEncoder encoder;
+	private final UserCacheRepository redisRepository;
 
 	@Value("${jwt.secret-key}")
 	private String secretKey;
@@ -26,10 +29,12 @@ public class UserService {
 	@Value("${jwt.token.expired-time-ms}")
 	private Long expiredTimeMs;
 
-	public User loadUserByUsername(String userName) {
-		return userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(() ->
-			new SnsApplicationException(ErrorCode.USER_NOT_FOUND,
-				String.format("%s not founded", userName)));
+
+	public User loadUserByUsername(String userName) throws UsernameNotFoundException {
+		return redisRepository.getUser(userName).orElseGet(
+			() -> userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(
+				() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("userName is %s", userName))
+			));
 	}
 
 	@Transactional
@@ -48,11 +53,11 @@ public class UserService {
 
 	public String login(String userName, String password) { // 토큰을 반환해야하기 때문에 반환값은 String
 		// 회원가입 여부 체크
-		UserEntity userEntity = userEntityRepository.findByUserName(userName)
-			.orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("userName is %s", userName)));
+		User savedUser = loadUserByUsername(userName);
+		redisRepository.setUser(savedUser);
 
 		// 회원가입이 완료된 유저라면, 비밀번호 체크
-		if(!encoder.matches(password, userEntity.getPassword())){// 등록된 (인코딩)비밀번호와 입력받은 비밀번호가 다를 경우
+		if(!encoder.matches(password, savedUser.getPassword())){// 등록된 (인코딩)비밀번호와 입력받은 비밀번호가 다를 경우
 			throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);// 에러 반환
 		}
 
